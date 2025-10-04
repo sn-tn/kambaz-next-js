56f6b73bd117ba717e70bb8d50123c1c5862ba85
sn-tn <tian.se@northeastern.edu>
2025-10-03 21:30:43 -0400

    Added missing Course Navigation landing pages.

---
+++ app/(Kambaz)/Courses/[cid]/Piazza/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Piazza() {
+  return (
+    <div>
+      <h1>Piazza</h1>
+    </div>
+  );
+}
---
+++ app/(Kambaz)/Courses/[cid]/Quizzes/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Quizzes() {
+  return (
+    <div>
+      <h1>Quizzes</h1>
+    </div>
+  )
+}

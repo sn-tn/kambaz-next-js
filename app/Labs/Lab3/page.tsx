760abf5d3f046d1449618457b39b0a0ed3888848
sn-tn <tian.se@northeastern.edu>
2025-10-17 21:49:56 -0400

    Finished lab 3. Table of contents now highlights dynamically.

--- app/Labs/Lab3/page.tsx
+++ app/Labs/Lab3/page.tsx
@@ -1,8 +1,73 @@
-function Lab3() {
+import Add from "./Add";
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import ArrowFunctions from "./ArrowFunctions";
+import BooleanVariables from "./BooleanVariables";
+import Classes from "./Classes";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInline from "./ConditionalOutputInline";
+import Destructing from "./Destructing";
+import DestructingImports from "./DestructingImports";
+import FilterFunction from "./FilterFunction";
+import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
+import ForLoops from "./ForLoops";
+import FunctionDestructing from "./FunctionDestructing";
+import Highlight from "./Highlight";
+import House from "./House";
+import IfElse from "./IfElse";
+import ImpliedReturn from "./ImpliedReturn";
+import JsonStringify from "./JsonStringify";
+import LegacyFunctions from "./LegacyFunctions";
+import MapFunction from "./MapFunction";
+import PathParameters from "./PathParameters";
+import SimpleArrays from "./SimpleArrays";
+import Spreader from "./Spreader";
+import Square from "./Square";
+import Styles from "./Styles";
+import TemplateLiterals from "./TemplateLiterals";
+import TernaryOperator from "./TernaryOperator";
+import TodoList from "./todos/TodoList";
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+
+export default function Lab3() {
     return (
-        <div>
-            <h2>Lab 3</h2>
+        <div id="wd-lab3">
+            <h3>Lab 3</h3>
+            <VariablesAndConstants />
+            <VariableTypes />
+            <BooleanVariables />
+            <IfElse />
+            <TernaryOperator />
+            <ConditionalOutputIfElse />
+            <ConditionalOutputInline />
+            <LegacyFunctions />
+            <ArrowFunctions />
+            <ImpliedReturn />
+            <TemplateLiterals />
+            <SimpleArrays />
+            <ArrayIndexAndLength />
+            <AddingAndRemovingToFromArrays />
+            <ForLoops />
+            <MapFunction />
+            <FindFunction />
+            <FindIndex />
+            <FilterFunction />
+            <JsonStringify />
+            <House />
+            <Spreader />
+            <Destructing />
+            <FunctionDestructing />
+            <DestructingImports />
+            <Classes />
+            <Styles />
+            <Add a={3} b={4} />
+            <h4>Square of 4</h4>
+            <Square>4</Square>
+            <Highlight>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum auctor porttitor sapien, eu posuere justo faucibus et. Curabitur nec arcu semper metus dapibus blandit in eget elit. Pellentesque sit amet pretium tellus. Praesent cursus libero risus, at facilisis massa tempor. </Highlight>
+            <PathParameters />
+            <TodoList />
         </div>
     );
 }
-export default Lab3;
--- app/Labs/TOC.tsx
+++ app/Labs/TOC.tsx
@@ -1,26 +1,26 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { Nav, NavItem, NavLink } from "react-bootstrap";
 export default function TOC() {
+    const pathname = usePathname();
+    const links = [
+        { href: "/Labs", label: "Labs", id: "wd-labs-link"},
+        { href: "/Labs/Lab1", label: "Lab1", id: "wd-lab-1-link"},
+        { href: "/Labs/Lab2", label: "Lab2", id: "wd-lab-2-link"},
+        { href: "/Labs/Lab3", label: "Lab3", id: "wd-lab-3-link"},
+        { href: "/", label: "Kambaz", id: "wd-kambaz-link"},
+        { href: "https://github.com/sn-tn/kambaz-next-js", label: "GitHub Repo", id: "wd-github-repo-link"}
+    ];
     return (
         <Nav variant="pills">
-            <NavItem>
-                <NavLink href="/Labs" as={Link}>Labs</NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink href="/Labs/Lab1" as={Link}>Lab 1</NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink href="/Labs/Lab2" as={Link}>Lab 2</NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink href="/Labs/Lab3" as={Link}>Lab 3</NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink href="/" as={Link}>Kambaz</NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink href="https://github.com/sn-tn/kambaz-next-js">GitHub Repo</NavLink>
-            </NavItem>
+            {links.map((link) => (
+                <NavItem key={link.id}>
+                    <NavLink as={Link} href={link.href} id={link.id} className={pathname.endsWith(link.href) ? "active" : ""}>
+                        {link.label}
+                    </NavLink>
+                </NavItem>
+            ))}
         </Nav>
     );
 }

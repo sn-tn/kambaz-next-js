265b9376ca50581cf4bf1d7c3f72591b188009ea
sn-tn <tian.se@northeastern.edu>
2025-11-04 01:45:35 -0500

    Implemented CourseNavigation toggle.

--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -1,21 +1,26 @@
-import { ReactNode } from "react";
+"use client";
+import { ReactNode, useState } from "react";
 import CourseNavigation from "./Navigation";
 import { FaAlignJustify } from "react-icons/fa6";
-import { courses } from "../../Database";
 import Breadcrumb from "./Breadcrumb";
+import { useParams } from "next/navigation";
+import { useSelector } from "react-redux";
 
-export default async function CoursesLayout({ children, params }: Readonly<{ children: ReactNode; params: Promise<{ cid: string }> }>) {
-  const { cid } = await params;
-  const course = courses.find((course) => course._id === cid);
+export default function CoursesLayout({ children }: { children: ReactNode }) {
+  const { cid } = useParams();
+  const { courses } = useSelector((state: any) => state.coursesReducer);
+  const course = courses.find((course: any) => course._id === cid);
+  const [ nav, setNav ]  = useState(true);
   return (
     <div id="wd-courses">
       <h2 className="text-danger">
-        <FaAlignJustify className="me-4 fs-4 mb-1" /> <Breadcrumb course={course}/>
+        <FaAlignJustify className="me-4 fs-4 mb-1" onClick={() => setNav(!nav)}/>
+        <Breadcrumb course={course} />
       </h2>
       <hr />
       <div className="d-flex">
         <div className="d-none d-sm-block">
-          <CourseNavigation />
+          {nav && <CourseNavigation />}
         </div>
         <div className="flex-fill">
           {children}

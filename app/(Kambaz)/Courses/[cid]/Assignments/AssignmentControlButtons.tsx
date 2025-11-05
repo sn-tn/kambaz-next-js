9e9f69c04fb2b5ad652414547e6760d9f9b48477
sn-tn <tian.se@northeastern.edu>
2025-11-05 01:42:02 -0500

    Fixed some Vercel Deployment issues.

--- app/(Kambaz)/Courses/[cid]/Assignments/AssignmentControlButtons.tsx
+++ app/(Kambaz)/Courses/[cid]/Assignments/AssignmentControlButtons.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react';
 import GreenCheckmark from './GreenCheckmark';
 import { IoEllipsisVertical } from 'react-icons/io5';

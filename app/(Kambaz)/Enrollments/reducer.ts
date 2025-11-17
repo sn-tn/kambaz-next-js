9e04b39e8eb142f6d2559f47183c00c9cb8b90e8
sn-tn <tian.se@northeastern.edu>
2025-11-17 17:15:40 -0500

    Created reducer for enrollments.

---
+++ app/(Kambaz)/Enrollments/reducer.ts
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  enrollments: [],
+};
+const enrollmentsSlice = createSlice({
+  name: "enrollments",
+  initialState,
+  reducers: {
+    setEnrollments: (state, action) => {
+      state.enrollments = action.payload;
+    }
+  },
+});
+export const { setEnrollments } = enrollmentsSlice.actions;
+export default enrollmentsSlice.reducer;

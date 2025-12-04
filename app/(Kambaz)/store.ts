484bf7843e30f3932afa713c8cb8443d4c322d81
sn-tn <tian.se@northeastern.edu>
2025-12-03 23:35:27 -0500

    Commented out store.ts enrollments reducer dependency.

--- app/(Kambaz)/store.ts
+++ app/(Kambaz)/store.ts
@@ -3,14 +3,14 @@ import coursesReducer from "./Courses/reducer";
 import modulesReducer from "./Courses/[cid]/Modules/reducer";
 import accountReducer from "./Account/reducer";
 import assignmentReducer from "./Courses/[cid]/Assignments/reducer";
-import enrollmentsReducer from "./Enrollments/reducer";
+// import enrollmentsReducer from "./Enrollments/reducer";
 const store = configureStore({
   reducer: {
     coursesReducer,
     modulesReducer,
     accountReducer,
     assignmentReducer,
-    enrollmentsReducer,
+    // enrollmentsReducer,
   },
 
 });

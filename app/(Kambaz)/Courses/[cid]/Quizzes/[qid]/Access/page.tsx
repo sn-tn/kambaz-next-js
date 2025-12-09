6c3c7a61e1c291ba05be523bfae98846ffca2420
sn-tn <tian.se@northeastern.edu>
2025-12-09 16:26:47 -0500

    Added label to access page.

--- app/(Kambaz)/Courses/[cid]/Quizzes/[qid]/Access/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Quizzes/[qid]/Access/page.tsx
@@ -2,32 +2,39 @@
 "use client";
 import { redirect, useParams } from "next/navigation";
 import { useEffect, useState } from "react";
-import { Button, FormControl } from "react-bootstrap";
+import { Button, FloatingLabel, FormControl } from "react-bootstrap";
 import * as client from "../../../../client";
 
 export default function QuizAccess() {
-  const {cid, qid} = useParams();
+  const { cid, qid } = useParams();
   const [code, setCode] = useState<string>("");
   const [quiz, setQuiz] = useState<any>();
   const fetchQuiz = async () => {
-    const quiz = await client.fetchQuizById(cid as string, qid as string)
+    const quiz = await client.fetchQuizById(cid as string, qid as string);
     setQuiz(quiz);
-  }
+  };
   const validate = () => {
     if (quiz.accessCode === code) {
       redirect(`/Courses/${cid}/Quizzes/${qid}/Preview`);
     }
-  }
+  };
   useEffect(() => {
     fetchQuiz();
-  })
+  });
   if (quiz && quiz.accessCode === "") {
     validate();
   }
   return (
     <div id="wd-quiz-access">
-      <FormControl onChange={(e) => setCode(e.target.value)}/>
-      <Button as="label" htmlFor="" onClick={validate}>Submit</Button>
+      <FloatingLabel label="Access Code">
+        <FormControl
+          id="wd-access-field"
+          onChange={(e) => setCode(e.target.value)}
+        />
+      </FloatingLabel>
+      <Button as="label" htmlFor="" onClick={validate}>
+        Submit
+      </Button>
     </div>
   );
 }
